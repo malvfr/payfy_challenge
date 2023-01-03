c8f49a6699d462c59dc7346ae85fcb45515827cb
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2023-01-03 20:02:27 -0300

    Adding main schema

---
+++ lib/payfy/schema.ex
@@ -0,0 +1,11 @@
+defmodule Payfy.Schema do
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
+      @primary_key {:id, :binary_id, autogenerate: true}
+      @foreign_key_type :binary_id
+
+      @type id :: binary
+    end
+  end
+end
