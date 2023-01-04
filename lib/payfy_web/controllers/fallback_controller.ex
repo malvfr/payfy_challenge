40366eb1865bea2622b76d9291ab3b8cb0ce30bd
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2023-01-04 14:40:28 -0300

    Error handling for fallback controller

--- lib/payfy_web/controllers/fallback_controller.ex
+++ lib/payfy_web/controllers/fallback_controller.ex
@@ -13,4 +13,11 @@ defmodule PayfyWeb.FallbackController do
     |> put_view(PayfyWeb.ErrorView)
     |> render(:"404")
   end
+
+  def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(PayfyWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
 end
