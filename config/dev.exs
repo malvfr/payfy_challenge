3ea99a0a269262f829884b574ec6ac1eacca2221
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2023-01-04 11:12:13 -0300

    TDD setup

--- config/dev.exs
+++ config/dev.exs
@@ -62,3 +62,5 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+config :mix_test_watch, clear: true
